2c672e15f58e3d631f6df0dba5e96e718403b6ff
Joao Vieira <joo.vieira123@gmail.com>
2020-11-24 14:08:40 +0100

    created the request form

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -1,13 +1,28 @@
 class RequestsController < ApplicationController
+  skip_before_action :authenticate_user!
+
   def index
   end
 
   def new
+    @request = Request.new
   end
 
   def create
+    @request = Request.new(request_params)
+    if @request.save
+      redirect_to request_path(@request)
+    else
+      render :new
+    end
   end
 
   def show
+  end
+
+  private
+
+  def request_params
+    params.require(:request).permit(:starting_location, :ending_location)
   end
 end
