d9be0e22b6a2246890574f7c80dd4c2b85bee644
Ashley Collins <ashleyjohnroycollins@gmail.com>
2020-11-26 16:05:16 +0100

    Update to user paths

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,17 +2,18 @@ class ReviewsController < ApplicationController
 
   def new
     @review = Review.new
+    @ride = Ride.find(params[:ride_id])
   end
 
   def create
-   @ride = ride.find(params[:ride_id])
-	 @review = Review.new(review_params)
- 	 @review.ride = @ride
- 	   if @review.save
- 		  link_to 'submit', root_path
- 	   else
- 		  render :new
- 	   end
+    @ride = Ride.find(params[:ride_id])
+	  @review = Review.new(review_params)
+ 	  @review.ride = @ride
+ 	    if @review.save
+ 		   redirect_to root_path
+ 	    else
+ 		   render :new
+ 	    end
   end
 
   private
