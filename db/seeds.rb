4b0a010510025ad8a3701ff256fa1d0f99734fce
Ashley Collins <ashleyjohnroycollins@gmail.com>
2020-11-27 16:08:48 +0100

    Seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,7 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-Request.destroy_all 
+Review.destroy_all
+Ride.destroy_all
+Request.destroy_all
 User.destroy_all
 user = User.create!(email: "las@wal.com", password: "123456")
 Request.create!(starting_location: 'Sodermalm', ending_location: 'future', user: user, status: 2)
